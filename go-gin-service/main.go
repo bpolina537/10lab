f3c9b5f32dad1fb599192e01d74998bd13dc261c
bpolina537 <bpolina771@gmail.com>
2026-04-03 18:31:11 +0300

    feat: add logging middleware

--- go-gin-service/main.go
+++ go-gin-service/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
+    "go-gin-service/middleware"
 )
 
 func main() {
     r := gin.Default()
+
+    // Подключаем middleware глобально
+    r.Use(middleware.Logger())
 
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
